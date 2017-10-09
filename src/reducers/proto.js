acf1954611f9672082818327e91897fdd0816f66
Oleksandr Kovalchuk <anxolerd@outlook.com>
2017-10-09 20:12:03 +0300

    Use a domain.Descriptor instead of one from grpclib

--- src/reducers/proto.js
+++ src/reducers/proto.js
@@ -1,11 +1,14 @@
+import { Descriptor } from '../lib/domain';
 import * as actions from '../actions';
 
 
 const proto = (state = { descriptor: null, filename: null }, action) => {
   switch (action.type) {
     case actions.PROTO.LOAD:
+      const grpcDescriptor = window.grpc.load(action.filename);
+      const descriptor = new Descriptor(grpcDescriptor);
       return Object.assign({}, state, {
-        descriptor: window.grpc.load(action.filename),
+        descriptor,
         filename: action.filename,
       });
     default:
