8689939c0f37299f2f7dd9039c9a2faec58935d7
Oleksandr Kovalchuk <anxolerd@outlook.com>
2017-10-15 20:49:57 +0300

    Style HostController component

--- src/components/HostController/index.js
+++ src/components/HostController/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import './index.css';
 
 
 const HostController = ({ onHostChange, onPortChange, host, port }) => (
   <div className='HostController'>
+    <div className='HostController-title'>
+      gRPC server host and port:
+    </div>
     <div className='HostController-hostInput'>
       <input
         className='HostController-hostInput-field'
-        type='url'
         value={host}
         onChange={(e) => onHostChange(e.target.value)}
         placeholder='GRPC server host'
       />
     </div>
+    <span>:</span>
     <div className='HostController-portInput'>
       <input
         className='HostController-portInput-field'
