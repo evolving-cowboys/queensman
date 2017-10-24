1191e30fec7c48b8a0ef5ad12db78460a02c2265
v.valko <v.valko@smartweb.com.ua>
2017-10-24 23:25:28 +0300

    [improve] introduce eslint with airbnb config, remove more react native configs

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -82,15 +82,7 @@ module.exports = {
     // We also include JSX as a common component filename extension to support
     // some tools, although we do not recommend using it, see:
     // https://github.com/facebookincubator/create-react-app/issues/290
-    // `web` extension prefixes have been added for better support
-    // for React Native Web.
-    extensions: ['.web.js', '.js', '.json', '.web.jsx', '.jsx'],
-    alias: {
-      
-      // Support React Native Web
-      // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-      'react-native': 'react-native-web',
-    },
+    extensions: ['.js', '.json', '.jsx'],
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
       // This often causes confusion because we only process files within src/ with babel.
